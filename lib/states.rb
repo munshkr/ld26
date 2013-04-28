d3c5a3ee9c4cf7bed7e53786c2b7b6165a72884b
Damián Silvani <munshkr@gmail.com>
2013-04-28 13:02:55 -0300

    advance with easing, advance automatically every 1000ms

--- lib/states.rb
+++ lib/states.rb
@@ -7,7 +7,8 @@ class Play < Chingu::GameState
   trait :timer
 
   ANGLE_STEP = 60
-  DURATION_ROTATION = 500
+  DURATION_ROTATION = 250
+  DURATION_ADVANCE  = 250
 
   def initialize
     super
@@ -21,7 +22,7 @@ class Play < Chingu::GameState
     @camera_y = 0
 
     every(1000, name: :move) do
-      #advance
+      advance
     end
 
     self.input = {
@@ -51,6 +52,23 @@ class Play < Chingu::GameState
       end
     end
 
+    if advancing?
+      t = Gosu::milliseconds - @advance_old_time
+      if t > DURATION_ADVANCE
+        @camera_x = @advance_old_camera_x + @advance_change_x
+        @camera_y = @advance_old_camera_y + @advance_change_y
+        @advance_change_x = @advance_change_y = nil
+        @player.advance
+      else
+        b = @advance_old_camera_x
+        c = @advance_change_x
+        @camera_x = ease_in_out_quad(t, b, c, DURATION_ADVANCE)
+        b = @advance_old_camera_y
+        c = @advance_change_y
+        @camera_y = ease_in_out_quad(t, b, c, DURATION_ADVANCE)
+      end
+    end
+
     $window.caption = "FPS #{$window.fps} - " \
       "milliseconds_since_last_tick: #{$window.milliseconds_since_last_tick} - " \
       "game objects #{current_game_state.game_objects.size}"
@@ -69,12 +87,14 @@ class Play < Chingu::GameState
   end
 
   def advance
-    if not rotating?
-      dist = Cell::RADIUS * Math.sqrt(3)
-      @camera_x += offset_x(90 - @camera_angle, dist)
-      @camera_y += offset_y(90 - @camera_angle, dist)
+    if not moving?
+      @advance_old_camera_x = @camera_x
+      @advance_old_camera_y = @camera_y
+      @advance_old_time = Gosu::milliseconds
 
-      @player.advance
+      dist = Cell::RADIUS * Math.sqrt(3)
+      @advance_change_x = offset_x(90 - @camera_angle, dist)
+      @advance_change_y = offset_y(90 - @camera_angle, dist)
     end
   end
 
@@ -85,7 +105,7 @@ class Play < Chingu::GameState
   end
 
   def rotate_left
-    if not rotating?
+    if not moving?
       @rotation_old_camera_angle = @camera_angle
       @rotation_old_time = Gosu::milliseconds
       @rotation_change = ANGLE_STEP
@@ -93,7 +113,7 @@ class Play < Chingu::GameState
   end
 
   def rotate_right
-    if not rotating?
+    if not moving?
       @rotation_old_camera_angle = @camera_angle
       @rotation_old_time = Gosu::milliseconds
       @rotation_change = -ANGLE_STEP
@@ -102,5 +122,13 @@ class Play < Chingu::GameState
 
   def rotating?
     !!@rotation_change
+  end
+
+  def advancing?
+    !!@advance_change_x
+  end
+
+  def moving?
+    rotating? or advancing?
   end
 end
